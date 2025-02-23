f362f028d23f4464a21c65ec54b7a3d640c537bb
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2025-02-23 14:48:15 +0530

    Credentials Replication Enabled for PostgreSQL

--- credentials/variables.tf
+++ credentials/variables.tf
@@ -1,7 +1,11 @@
+# ------------ MINIO VARIABLES ------------ #
+
 variable "namespace" {
   default     = "minio"
   description = "Namespace to be used for deploying MinIO Tenant and related resources."
 }
+
+# ------------ POSTGRESQL VARIABLES ------------ #
 
 variable "postgres_service_account_name" {
   default     = "postgres-access"
@@ -11,4 +15,9 @@ variable "postgres_service_account_name" {
 variable "postgres_backups_access_credentials" {
   default     = "postgres-access-key"
   description = "Secret name for PG Backups Access Keys"
+}
+
+variable "postgresql_namespace" {
+  default     = "postgres"
+  description = "Namespace to be used with the PostgreSQL database and related resources"
 }
